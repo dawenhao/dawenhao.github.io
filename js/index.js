d60d5995def3cf644ff8d37b0fa61b51d200fefc
dawenhao <773509486@qq.com>
2018-06-06 22:55:59 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,12 +2,62 @@ $(function(){
 	Init();
 });
 
-function Init() {
-	var geolocation = new qq.maps.Geolocation("Z4UBZ-UHNWF-Q4AJQ-NVQ7P-FURZ7-3AFRT", "myWeb");
-	geolocation.getLocation(showPosition, null);
-};
+var xhr;
 
-function showPosition(position) {
-	var a = JSON.stringify(position, null, 4);
-	alert(a);
-};
+//function Init() {
+//	var geolocation = new qq.maps.Geolocation("Z4UBZ-UHNWF-Q4AJQ-NVQ7P-FURZ7-3AFRT", "myWeb");
+//	geolocation.getLocation(showPosition, null);
+//};
+//
+//function showPosition(position) {
+//	var a = JSON.stringify(position, null, 4);
+//	alert(a);
+//};
+
+function Init() {
+	$.ajax({
+		type:"get",
+		url:"http://v.juhe.cn/toutiao/index",
+		data:{
+			type:'top',
+			key:'14464207dad298fc22fd9f685cb55954'
+		},
+		dataType:'json',
+		success:function(data){
+			alert(data);
+		},
+		error:function(error,Msgerror){
+			alert("错误"+error);
+		}
+	});
+//	mui.ajax('http://v.juhe.cn/toutiao/index',{
+//		data:{
+//			type:'top',
+//			key:'14464207dad298fc22fd9f685cb55954'
+//		},
+//		dataType:'json',//服务器返回json格式数据
+//		type:'get',//HTTP请求类型
+//		timeout:10000,//超时时间设置为10秒；
+//		headers:{'Content-Type':'application/json'},
+//		success:function(data){
+//			//服务器返回响应，根据响应结果，分析是否登录成功；
+//			alert(data);
+//		},
+//		error:function(xhr,type,errorThrown){
+//			//异常处理；
+//			console.log(type);
+//		}
+//	});
+
+//	xhr = new XMLHttpRequest;
+//	xhr.onreadystatechange = Callback;
+// 	xhr.open("get", "http://v.juhe.cn/toutiao/index?type=top&&key=14464207dad298fc22fd9f685cb55954" ,true);
+// 	xhr.send(null);
+}
+
+//function Callback(){
+//	if(xhr.readyState == 4 && xhr.status == 200){
+//		var data = xhr.responseText;
+//		alert(data);
+//	}
+//}
